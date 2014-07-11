0fb208d3481c2e57d7b080aac6b606b091f250e3
Josh Gillies <github@joshgilli.es>
2014-07-12 02:44:23 +1000

    Fix broken pipe

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fetchr = require('./lib/fetchr');
 var searches = require('./lib/recentSearches');
 var PouchDB = require('pouchdb');
-var db = new PouchDB(process.env.COUCHDB | 'local');
+var db = new PouchDB(process.env.COUCHDB || 'local');
 
 fetchr.fetchEvery('http://trove.nla.gov.au/recentSearches', 3000, function (err, html) {
   if (err) return console.error(err);
