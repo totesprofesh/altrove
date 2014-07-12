52cf95bc2f7eb808f4843d758e87164d8d2b7ce1
Matt McKellar-Spence <matt@mattms.com>
2014-07-12 20:05:18 +1000

    Add random picture route and Trove query

---
+++ routes/picture.js
@@ -0,0 +1,46 @@
+var url = require('url');
+var trove_search = require('../lib/trove_search');
+
+
+exports.get_random_pictures = function (req, res, next) {
+	var query = url.parse(req.url, true).query;
+	console.log(query)
+
+	var args = {
+		key: process.env.TROVE_KEY,
+		'l-availability': 'y/f',
+		'l-format': 'Photograph',
+		zone: 'picture'
+	};
+
+	var count = parseInt(query.count);
+	if (!count || count < 1) {
+		count = 10;
+	} else {
+		count = Math.min(count, 100);
+	}
+
+	var fixed_works = [];
+
+	trove_search.get_random_works(args, count, function (err, obj) {
+		if (err) return next(err);
+
+		var works = obj.response.zone[0].records.work;
+		var ids;
+
+		works.forEach(function (work) {
+			ids = work.identifier;
+			if (ids && ids.length > 1 && ids[1].linktype == 'thumbnail') {
+				fixed_works.push({
+					i: ids[1].value,
+					l: work.troveUrl,
+				});
+			}
+		});
+
+		res.send({
+			image: fixed_works
+		});
+		return next();
+	});
+};
